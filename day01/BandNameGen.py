7cf939a08a6b599f01f99f54d29acd7d695b77ac
UHuang <uhuang@users.noreply.github.com>
2022-04-09 21:04:16 -0400

    ultra basic initial attempt with no checks

---
+++ day01/BandNameGen.py
@@ -0,0 +1,8 @@
+# Band Name Generator
+
+print("Welcome to the Band Name Generator.")
+
+city = input("What's the name of the city you grew up in?")
+pet = input("What's your pet's name?")
+
+print(f"Your band name could be {city} {pet}")
