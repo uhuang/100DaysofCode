c819e7f669d3c329db127314355eaf05e9248e6c
UHuang <uhuang@users.noreply.github.com>
2022-04-12 23:36:24 -0400

    odd/even check exercise

---
+++ day03/oddoreven.py
@@ -0,0 +1,8 @@
+# This program checks whether a provided number is odd or even
+
+number = int(input("Which number do you want to check? "))
+
+if number % 2 == 0:
+    print("This number is even.")
+else:
+    print("This number is odd.")
