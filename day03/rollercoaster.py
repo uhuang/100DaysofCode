a8aef7ea61cda0b68258086d0ae8bce220f00f90
UHuang <uhuang@users.noreply.github.com>
2022-04-12 23:31:09 -0400

    rollercoaster lecture

---
+++ day03/rollercoaster.py
@@ -0,0 +1,7 @@
+print("Welcome to the rollercoaster! You must be at least 120 cm to ride.")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry, you are too short to ride the rollercoaster.")
