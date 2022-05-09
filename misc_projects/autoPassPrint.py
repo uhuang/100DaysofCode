b7048f7672aa0332d288e7c025b24f2c8c1b336d
uhuang <uhuang@users.noreply.github.com>
2022-05-09 10:00:41 -0400

    new modifications

--- misc_projects/autoPassPrint.py
+++ misc_projects/autoPassPrint.py
@@ -8,15 +8,16 @@ Required: Python 3.2 or higher, pyautogui (can be installed via pip)
 
 import subprocess
 import sys
-import tkinter
+import unittest
 import pyautogui
+import tkinter
 from tkinter import *
 from tkinter import ttk
 
-CONTACT_PERSON = "CONTACT"
+CONTACT_PERSON = "CONTACT INFO"
 BROWSER_LOCATION = "PATH TO BROWSER"
 BROWSER_SELECT = "BROWSER"
-SCREENSHOT_FOLDER = "PATH TO SCREENSHOT FOLDER"
+SCREENSHOT_FOLDER = "PATH TO SCREENSHOTS"
 SITE_LINK = "LINK"
 
 try:
@@ -48,10 +49,10 @@ try:
             elif NUM_PASSES < 0:
                 pyautogui.alert("Can't print a negative number of items. Exiting.")
                 sys.exit()
-            elif NUM_PASSES >= 20:
+            elif NUM_PASSES > 20:
                 pyautogui.alert(
                     f"You have requested a large number of passes ({NUM_PASSES}).\
-                    \nPlease try entering a smaller number (less than 20)."
+                    \nPlease try entering a smaller number (20 or less)."
                 )
                 continue
             elif NUM_PASSES >= 1:
