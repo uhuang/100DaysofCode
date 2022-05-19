e63e4bfe9d87548ea61d696b0e98bd59eaa9bc50
UHuang <uhuang@users.noreply.github.com>
2022-05-18 20:38:24 -0400

    back to this after a bit of a break

---
+++ day03/leap_year.py
@@ -0,0 +1,12 @@
+# 🚨 Don't change the code below 👇
+year = int(input("Which year do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+
+
+# How to tell if a year is a leap year:
+# on every year that is evenly divisible by 4
+# **except** every year that is evenly divisible by 100
+# **unless** the year is also evenly divisible by 400
